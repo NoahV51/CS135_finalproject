1acb9b9850bc33e7768b18d6a94f5ec73453df53
gmarchese3 <thegmarch@gmail.com>
2014-12-01 21:31:30 -0800

    Update si.cpp

--- si.cpp
+++ si.cpp
@@ -1,19 +1,84 @@
+/*----------------------------------------------
+                 HEADERS
+------------------------------------------------*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
 
-//menu constants 
-const int menu_min_x = 10;
-const int menu_min_y = 3;
-const int menu_max_x = 66;
-const int menu_max_y = 20;
 
 
 
-//prototypes
+/*----------------------------------------------
+                PROTOTYPES
+------------------------------------------------*/
 
+void initialize();
+
+void makeEraseString(char buf[], int len);
+
+void printWelcomeScreen(int maxy, int maxx);
+void printInstructions(int maxy, int maxx);
+void printAliens();
+void printBarriers();
+
+void finish(int sig);
+
+
+/*----------------------------------------------
+                MAIN FUNCTION
+------------------------------------------------*/
+int main(int argc, char *argv[]){
+
+    //Initialize Variables
+    int maxx, maxy; //maxx and maxy size of terminal
+    double tankPositionX = maxx - 70 ;
+    double tankPositionY = maxy - 2;
+    //Start Curses
+    initialize();
+    getmaxyx(stdscr, maxy, maxx);      // get current screen size 
+     
+
+       //Display Tittle Screen
+       printWelcomeScreen(maxy, maxx);    // Print welcome screen
+       erase();
+   
+       printInstructions(maxy, maxx);
+       erase();
+
+     //Start Loop  
+
+     //print aliens 
+     printAliens();
+     
+     //print barriers
+     printBarriers();
+     erase();
+     char in;
+     scanf("%c", &in);
+
+ 
+
+      //print menu screen 
+
+      //if '1' , show score board 
+
+      //else if '2' , play the game
+
+      
+      //End Curses
+      finish(0);
+     
+      //End Program 
+      return 0;
+
+}
+
+
+/*----------------------------------------------
+                 FUNCTIONS
+------------------------------------------------*/
 
 void initialize(){
  //terminal graphics
@@ -27,15 +92,22 @@ void initialize(){
  init_pair(2, COLOR_RED, COLOR_BLACK); 
 
  //initialize game global variables
- float speed_shotX = 0.0; 
- float speed_shotY = 0.0005; //speed of the bullet in y dir.
+ 
 
- float tankVelocityX = 0.0001; 
- float tankVelocityY = 0.0;
- float tankVelocityChage = 0.0005;
+ //tank
+ double tankVelocityX = 0.0001; 
+ double tankVelocityY = 0.0;
+ double tankVelocityChage = 0.0005;
+// double tankPositionX = maxx - 70 ;
+// double tankPositionY = maxy - 2;
+ double speed_shotX = 0.0; 
+ double speed_shotY = 0.0005; //speed of the bullet in y dir.
 
- float alienVelocityX = 0.0001;
- float alienVelocityY = 0.0;
+ //alien
+ double alienVelocityX = 0.0001;
+ double alienVelocityY = 0.0;
+ //double alienPositionX = ;
+ //double alienPositionY = ;
 }
 
 
@@ -48,19 +120,244 @@ void makeEraseString(char buf[], int len){
   return;
 }
 
+void printBarriers(){
+
+ //set barrier color 
+ attron(COLOR_PAIR(1));
+
+ //print the barriers
+ mvaddch(19,5, '#');
+ mvaddch(19,6, '#');
+ mvaddch(19,7, '#');
+ mvaddch(19,8, '#');
+ mvaddch(19,9, '#');
+ mvaddch(19,10, '#');
+ mvaddch(19,11, '#');
+ mvaddch(19,12, '#');
+
+ mvaddch(19,20, '#');
+ mvaddch(19,21, '#');
+ mvaddch(19,22, '#');
+ mvaddch(19,23, '#');
+ mvaddch(19,24, '#');
+ mvaddch(19,25, '#');
+ mvaddch(19,26, '#');
+ mvaddch(19,27, '#');
+
+ mvaddch(19,36, '#');
+ mvaddch(19,37, '#');
+ mvaddch(19,38, '#');
+ mvaddch(19,39, '#');
+ mvaddch(19,40, '#');
+ mvaddch(19,41, '#');
+ mvaddch(19,42, '#');
+ mvaddch(19,43, '#');
+
+ mvaddch(19,51, '#');
+ mvaddch(19,52, '#');
+ mvaddch(19,53, '#');
+ mvaddch(19,54, '#');
+ mvaddch(19,55, '#');
+ mvaddch(19,56, '#');
+ mvaddch(19,57, '#');
+ mvaddch(19,58, '#');
+
+ mvaddch(19,67, '#');
+ mvaddch(19,68, '#');
+ mvaddch(19,69, '#');
+ mvaddch(19,70, '#');
+ mvaddch(19,71, '#');
+ mvaddch(19,72, '#');
+ mvaddch(19,73, '#');
+ mvaddch(19,74, '#');
+
+ mvaddch(18,6, '#');
+ mvaddch(18,7, '#');
+ mvaddch(18,8, '#');
+ mvaddch(18,9, '#');
+ mvaddch(18,10, '#');
+ mvaddch(18,11, '#');
+ mvaddch(18,21, '#');
+ mvaddch(18,22, '#');
+ mvaddch(18,23, '#');
+ mvaddch(18,24, '#');
+ mvaddch(18,25, '#');
+ mvaddch(18,26, '#');
+
+ mvaddch(18,37, '#');
+ mvaddch(18,38, '#');
+ mvaddch(18,39, '#');
+ mvaddch(18,40, '#');
+ mvaddch(18,41, '#');
+ mvaddch(18,42, '#');
+
+ mvaddch(18,52, '#');
+ mvaddch(18,53, '#');
+ mvaddch(18,54, '#');
+ mvaddch(18,55, '#');
+ mvaddch(18,56, '#');
+ mvaddch(18,57, '#');
+
+ mvaddch(18,68, '#');
+ mvaddch(18,69, '#');
+ mvaddch(18,70, '#');
+ mvaddch(18,71, '#');
+ mvaddch(18,72, '#');
+ mvaddch(18,73, '#');
+
+ mvaddch(17,7, '#');
+ mvaddch(17,8, '#');
+ mvaddch(17,9, '#');
+ mvaddch(17,10, '#');
+
+ mvaddch(17,22, '#');
+ mvaddch(17,23, '#');
+ mvaddch(17,24, '#');
+ mvaddch(17,25, '#');
+
+ mvaddch(17,38, '#');
+ mvaddch(17,39, '#');
+ mvaddch(17,40, '#');
+ mvaddch(17,41, '#');
+
+ mvaddch(17,53, '#');
+ mvaddch(17,54, '#');
+ mvaddch(17,55, '#');
+ mvaddch(17,56, '#');
+
+ mvaddch(17,69, '#');
+ mvaddch(17,70, '#');
+ mvaddch(17,71, '#');
+ mvaddch(17,72, '#');
+
+ refresh(); //refresh screen
+ attroff(COLOR_PAIR(1)); // go back to default colors
+
+
+return;
+}
+
 
 
 void printWelcomeScreen(int maxy, int maxx){
   erase(); // clear screen
   attron(COLOR_PAIR(1));  //change color
-  mvprintw(maxy/2, maxx/2 - 10, "Welcome to NOT PONG"); // print welcome, approximately middle of screen
+  mvprintw(maxy/4, maxx/2 - 15, "Welcome to !(Space Invaders)"); // print welcome, approximately middle of screen
+  mvprintw(maxy/4+1, maxx/2 - 15, "----------------------------");
+  mvprintw(maxy/4+15, maxx/2 - 15, "press any key to continue...");
   refresh(); //refresh screen
   attroff(COLOR_PAIR(1)); // go back to default colors
-  sleep(1); // sleep for a second by calling the linux/unix sleep library call
+  //sleep(1); // sleep for a second by calling the linux/unix sleep library call
   char in;
   scanf("%c", &in);	
   return;
 }
+
+
+
+void printAliens(){
+ 
+ //set alien color
+ attron(COLOR_PAIR(2));
+ //column one
+ mvaddch(12,22, 'x');
+ mvaddch(9,22, 'a');
+ mvaddch(6,22, 'v');
+ mvaddch(3,22, 'k');
+ //Column two
+ mvaddch(12,23, 'x');
+ mvaddch(9,23, 'a');
+ mvaddch(6,23, 'v');
+ mvaddch(3,23, 'k');
+ //column three
+ mvaddch(12,24, 'x');
+ mvaddch(9,24, 'a');
+ mvaddch(6,24, 'v');
+ mvaddch(3,24, 'k');
+ //column four
+ mvaddch(12,30, 'x');
+ mvaddch(9,30, 'a');
+ mvaddch(6,30, 'v');
+ mvaddch(3,30, 'k');
+ //column five
+ mvaddch(12,31, 'x');
+ mvaddch(9,31, 'a');
+ mvaddch(6,31, 'v');
+ mvaddch(3,31, 'k');
+ //column six
+ mvaddch(12,32, 'x');
+ mvaddch(9,32, 'a');
+ mvaddch(6,32, 'v');
+ mvaddch(3,32, 'k');
+ //column seven
+ mvaddch(12,38, 'x');
+ mvaddch(9,38, 'a');
+ mvaddch(6,38, 'v');
+ mvaddch(3,38, 'k');
+ //column eight
+ mvaddch(12,39, 'x');
+ mvaddch(9,39, 'a');
+ mvaddch(6,39, 'v');
+ mvaddch(3,39, 'k');
+ //column nine
+ mvaddch(12,40, 'x');
+ mvaddch(9,40, 'a');
+ mvaddch(6,40, 'v');
+ mvaddch(3,40, 'k');
+ //column ten
+ mvaddch(12,46, 'x');
+ mvaddch(9,46, 'a');
+ mvaddch(6,46, 'v');
+ mvaddch(3,46, 'k');
+ //column eleven
+ mvaddch(12,47, 'x');
+ mvaddch(9,47, 'a');
+ mvaddch(6,47, 'v');
+ mvaddch(3,47, 'k');
+ //column twelve
+ mvaddch(12,48, 'x');
+ mvaddch(9,48, 'a');
+ mvaddch(6,48, 'v');
+ mvaddch(3,48, 'k');
+ //column thirteen
+ mvaddch(12,54, 'x');
+ mvaddch(9,54, 'a');
+ mvaddch(6,54, 'v');
+ mvaddch(3,54, 'k');
+ //column fourteen
+ mvaddch(12,55, 'x');
+ mvaddch(9,55, 'a');
+ mvaddch(6,55, 'v');
+ mvaddch(3,55, 'k');
+ //column fifteen
+ mvaddch(12,56, 'x');
+ mvaddch(9,56, 'a');
+ mvaddch(6,56, 'v');
+ mvaddch(3,56, 'k');
+
+ return;
+}
+
+
+
+
+void printInstructions(int maxy, int maxx){
+ erase(); // clear screen
+ attron(COLOR_PAIR(1));  //change color
+ mvprintw(maxy/4, maxx/2 - 10, "INSTRUCTIONS"); // print instructions, approximately middle of screen
+ mvprintw(maxy/4+1, maxx/2 - 10, "------------"); 
+ mvprintw(maxy/4+3, maxx/2 - 15, "Move: Arrows(Left/Right)");
+ mvprintw(maxy/4+4, maxx/2 - 15, "Shoot: Spacebar");
+ mvprintw(maxy/4+15, maxx/2 - 15, "press any key to continue...");
+ 
+ refresh(); //refresh screen
+ attroff(COLOR_PAIR(1)); // go back to default colors
+ //sleep(1); // sleep for a second by calling the linux/unix sleep library call
+ char in;
+ scanf("%c", &in);
+ return;
+}
+
 
 
 void finish(int sig)
@@ -76,20 +373,9 @@ void finish(int sig)
 
 
 
-//mainfunction
-int main(int argc, char *argv[]){
 
-    //Initialize Variables
-    int maxx, maxy; //maxx and maxy size of terminal
-
-    //Start Curses
-    initialize();
-    getmaxyx(stdscr, maxy, maxx);      // get current screen size 
-    printWelcomeScreen(maxy, maxx);    // Print welcome screen
-    erase();   
-
-
-
+//draw something 
+/*
 attron(COLOR_PAIR(2));
 mvaddch(5,5, 'x');
 mvaddch(4,5, 'x');
@@ -102,50 +388,7 @@ refresh(); //refresh screen
      //Display Tittle Screen
 	  printWelcomeScreen(maxy, maxx); 
 scanf("%c", &in);
-     //Begin Menu Loop
-
-
-       //print menu screen 
-
-	 //if '1' , show score board 
-
-	 //else if '2' , play the game
-
-      
-      //End Curses
-      finish(0);
-     
-      //End Program 
-      return 0;
-
-}
-
-//draw something 
-/*
-attron(COLOR_PAIR(1));
-mvaddch(5,5, 'x');
-mvaddch(4,5, 'x');
-mvaddch(3,5, 'x');
-refresh(); //refresh screen
-  attroff(COLOR_PAIR(1)); // go back to default colors
-  sleep(1);
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
